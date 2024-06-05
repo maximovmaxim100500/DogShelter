48a9ae6c424116a4265df05079b57cb5d3295ec6
Denis Siliukov <abhem21rus@mail.ru>
2024-06-05 09:56:05 +0300

    Initial commit

---
+++ src/main/java/com/assistance/DogShelter/DogShelterApplication.java
@@ -0,0 +1,12 @@
+package com.assistance.DogShelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogShelterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DogShelterApplication.class, args);
+	}
+
+}
