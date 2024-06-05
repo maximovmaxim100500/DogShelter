56bb78a2376b342506c77f239487783902254992
Denis Siliukov <abhem21rus@mail.ru>
2024-06-05 10:42:07 +0300

    - Added Maven Wrapper files  - Added Telegram bot functionality: greeting

---
+++ src/main/java/com/assistance/DogShelter/service/TelegramBot.java
@@ -0,0 +1,61 @@
+package com.assistance.DogShelter.service;
+
+import com.assistance.DogShelter.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Slf4j
+public class TelegramBot extends TelegramLongPollingBot {
+    private final BotConfig botConfig;
+
+    public TelegramBot(@Autowired BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default: sendMessage(chatId, "Sorry, required command is not recognized");
+
+            }
+        }
+    }
+    public void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + "! Nice to meet you!";
+        log.info("Replied to " + name);
+        sendMessage(chatId, answer);
+    }
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred while sending message: " + e.getMessage(), e);
+        }
+    }
+}
