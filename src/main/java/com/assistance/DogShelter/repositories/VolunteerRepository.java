4f77288fef96910dc78d8a5b86b4203e5273f5e1
Denis Siliukov <abhem21rus@mail.ru>
2024-06-12 11:54:57 +0300

    -Изменен VolunteerRepository (аннотация)

--- src/main/java/com/assistance/DogShelter/repositories/VolunteerRepository.java
+++ src/main/java/com/assistance/DogShelter/repositories/VolunteerRepository.java
@@ -2,9 +2,10 @@ package com.assistance.DogShelter.repositories;
 
 import com.assistance.DogShelter.model.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
     Optional<Volunteer> findByChatId(long chatId);
 
