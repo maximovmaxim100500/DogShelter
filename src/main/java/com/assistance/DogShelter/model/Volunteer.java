dfb2c9cbb9e8bea548667df2f6102664ae8769fc
Denis Siliukov <abhem21rus@mail.ru>
2024-06-12 11:54:08 +0300

    -Добавлена зависимость для PostgreSQL -Изменения в application.properties -Созданы файлы application-dsiliukov.properties, application-dkhan.properties, application-mmaksimov.properties (заполнен только dsiliukov) -Изменен tables.sq -Изменен VolunteerRepository -Изменен Volunteer

--- src/main/java/com/assistance/DogShelter/model/Volunteer.java
+++ src/main/java/com/assistance/DogShelter/model/Volunteer.java
@@ -1,77 +1,26 @@
 package com.assistance.DogShelter.model;
 
-
 // Модель, представляющая волонтера
 
 import jakarta.persistence.*;
-
-import java.util.Objects;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "volunteers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Volunteer {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     private long id;              // id волонтера
+    @Column(name = "chat_id", nullable = false)
     private long chatId;          // id волонтера в Telegram
+    @Column(name = "volunteer_name", nullable = false)
     private String name;          // имя волонтера
+    @Column(name = "status", nullable = false)
     private boolean isBusy;       //статус волонтёра (занят)
-
-    public Volunteer() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getChatId() {
-        return chatId;
-    }
-
-    public void setChatId(long chatId) {
-        this.chatId = chatId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isBusy() {
-        return isBusy;
-    }
-
-    public void setBusy(boolean busy) {
-        isBusy = busy;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Volunteer volunteer = (Volunteer) o;
-        return id == volunteer.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Volunteer{" +
-                "id=" + id +
-                ", chatId=" + chatId +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
--- src/main/resources/liquibase/scripts/tables.sql
+++ src/main/resources/liquibase/scripts/tables.sql
@@ -2,8 +2,10 @@
 
 -- changeset dkhan:1
 
-CREATE TABLE volunteers(
-id BIGSERIAL primary key,
-chat_id BIGINT,
-name VARCHAR
-)
+-- Создание таблицы для хранения информации о волонтерах
+CREATE TABLE volunteers (
+    id BIGSERIAL PRIMARY KEY,                -- Уникальный идентификатор волонтера
+    chat_id BIGINT UNIQUE NOT NULL,          -- Идентификатор чата в Telegram (обязательное поле, уникальное)
+    name VARCHAR(255) NOT NULL,              -- Имя волонтера (обязательное поле)
+    is_busy BOOLEAN DEFAULT FALSE            -- Статус занятости волонтера (по умолчанию - свободен)
+);
