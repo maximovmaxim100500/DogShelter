3f1eb99c4be232d161ea411c3adbe1bfba58cf70
Denis Siliukov <abhem21rus@mail.ru>
2024-07-14 18:43:38 +0300

    - Правки в тесте UserController, все проходит.

--- src/test/java/com/assistance/DogShelter/controller/MVCUserControllerTest.java
+++ src/test/java/com/assistance/DogShelter/controller/MVCUserControllerTest.java
@@ -1,112 +1,174 @@
 package com.assistance.DogShelter.controller;
 
+
 import com.assistance.DogShelter.db.model.User;
 import com.assistance.DogShelter.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@WebMvcTest(UserController.class)class MVCUserControllerTest {
+@WebMvcTest(UserController.class)
+class MVCUserControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private UserService userService;
 
-    @Autowired
-    private User user;
+    @Test
+    public void getUserByIdTest() throws Exception {
+        Long id = 1L;
+        String name = "Иван Иванов";
+        Long chatId = 123456789L;
+        String phoneNumber = "1234567890";
 
-    @BeforeEach
-    void setUp() {
-        user = new User();
-        user.setId(1L);
-        user.setName("Jane Doe");
-        user.setChatId(123456L);
-        user.setPhoneNumber("123-456-7890");
-        // установите другие свойства пользователя по необходимости
+        User expectedUser = new User();
+        expectedUser.setId(id);
+        expectedUser.setName(name);
+        expectedUser.setChatId(chatId);
+        expectedUser.setPhoneNumber(phoneNumber);
+
+        when(userService.findUserById(id)).thenReturn(Optional.of(expectedUser));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/users/{id}", id)
+                        .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                        .accept(org.springframework.http.MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.chatId").value(chatId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.phoneNumber").value(phoneNumber));
     }
 
     @Test
-    void testAddUser() throws Exception {
-        Mockito.when(userService.addUser(any(User.class))).thenReturn(user);
+    void getAllUsersTest() throws Exception {
+        // Создаем и сохраняем пользователей в сервисе или базе данных
+        List<User> users = Arrays.asList(
+                new User(1L, 12345778L, "Иван Иванов", "8-800-600-30-05", null, null),
+                new User(2L, 12435778L, "Петр Петров", "8-800-500-30-05", null, null)
+        );
+        when(userService.getAllUsers()).thenReturn(users);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/users/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\": \"Jane Doe\", \"chatId\": 123456, \"phoneNumber\": \"123-456-7890\"}")) // обновите JSON в соответствии с вашим объектом User
+        mockMvc.perform(get("/users/all")
+                        .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                        .accept(org.springframework.http.MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(2))); // Проверяем, что вернулась коллекция из двух пользователей
+    }
+
+    @Test
+    public void createUserTest() throws Exception {
+        Long id = 1L;
+        String name = "Иван Иванов";
+        Long chatId = 123456789L;
+        String phoneNumber = "1234567890";
+
+        JSONObject userObject = new JSONObject();
+        userObject.put("name", name);
+        userObject.put("chatId", chatId);
+        userObject.put("phoneNumber", phoneNumber);
+
+        User newUser = new User();
+        newUser.setId(id);
+        newUser.setName(name);
+        newUser.setChatId(chatId);
+        newUser.setPhoneNumber(phoneNumber);
+
+        when(userService.addUser(any(User.class))).thenReturn(newUser);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users/add")
+                        .content(userObject.toString())
+                        .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                        .accept(org.springframework.http.MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(1L))
-                .andExpect(jsonPath("$.name").value("Jane Doe"))
-                .andExpect(jsonPath("$.chatId").value(123456L))
-                .andExpect(jsonPath("$.phoneNumber").value("123-456-7890"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(id.intValue()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.chatId").value(chatId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.phoneNumber").value(phoneNumber));
     }
 
     @Test
-    void testFindUserById() throws Exception {
-        Mockito.when(userService.findUserById(anyLong())).thenReturn(Optional.of(user));
+    public void deleteUserTest() throws Exception {
+        Long id = 1L;
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/users/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1L))
-                .andExpect(jsonPath("$.name").value("Jane Doe"))
-                .andExpect(jsonPath("$.chatId").value(123456L))
-                .andExpect(jsonPath("$.phoneNumber").value("123-456-7890"));
-    }
+        // Настройка моков
+        User mockUser = new User();
+        mockUser.setId(id);
+        when(userService.findUserById(id)).thenReturn(Optional.of(mockUser));
+        doNothing().when(userService).deleteUser(id);
 
-    @Test
-    void testEditUser() throws Exception {
-        Mockito.when(userService.findUserById(anyLong())).thenReturn(Optional.of(user));
-        Mockito.when(userService.editUser(any(User.class))).thenReturn(user);
-
-        mockMvc.perform(MockMvcRequestBuilders.put("/users/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\": \"Jane Doe\", \"chatId\": 123456, \"phoneNumber\": \"123-456-7890\"}")) // обновите JSON в соответствии с вашим объектом User
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1L))
-                .andExpect(jsonPath("$.name").value("Jane Doe"))
-                .andExpect(jsonPath("$.chatId").value(123456L))
-                .andExpect(jsonPath("$.phoneNumber").value("123-456-7890"));
-    }
-
-    @Test
-    void testDeleteUser() throws Exception {
-        Mockito.when(userService.findUserById(anyLong())).thenReturn(Optional.of(user));
-
-        mockMvc.perform(MockMvcRequestBuilders.delete("/users/1"))
+        // Вызов и проверка
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/users/{id}", id)
+                        .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                        .accept(org.springframework.http.MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void testDeleteUserByChatId() throws Exception {
-        Mockito.when(userService.findUserByChatId(anyLong())).thenReturn(Optional.of(user));
+    public void deleteUserByChatIdTest() throws Exception {
+        Long chatId = 123456789L;
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/users/chatId/123456"))
+        // Настройка моков
+        User mockUser = new User();
+        mockUser.setChatId(chatId);
+        when(userService.findUserByChatId(chatId)).thenReturn(Optional.of(mockUser));
+        doNothing().when(userService).deleteUserChatId(chatId);
+
+        // Вызов и проверка
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/users/chatId/{chatId}", chatId)
+                        .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                        .accept(org.springframework.http.MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void testGetAllUsers() throws Exception {
-        Mockito.when(userService.getAllUsers()).thenReturn(Arrays.asList(user));
+    public void editUserTest() throws Exception {
+        Long id = 1L;
+        String name = "Иван Иванов";
+        Long chatId = 123456789L;
+        String phoneNumber = "1234567890";
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/users/all"))
+        JSONObject userObject = new JSONObject();
+        userObject.put("name", name);
+        userObject.put("chatId", chatId);
+        userObject.put("phoneNumber", phoneNumber);
+
+        User updatedUser = new User();
+        updatedUser.setId(id);
+        updatedUser.setName(name);
+        updatedUser.setChatId(chatId);
+        updatedUser.setPhoneNumber(phoneNumber);
+
+        when(userService.findUserById(id)).thenReturn(Optional.of(updatedUser));
+        when(userService.editUser(any(User.class))).thenReturn(updatedUser);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/users/{id}", id)
+                        .content(userObject.toString())
+                        .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                        .accept(org.springframework.http.MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(1L))
-                .andExpect(jsonPath("$[0].name").value("Jane Doe"))
-                .andExpect(jsonPath("$[0].chatId").value(123456L))
-                .andExpect(jsonPath("$[0].phoneNumber").value("123-456-7890"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(id.intValue()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.chatId").value(chatId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.phoneNumber").value(phoneNumber));
     }
 }
